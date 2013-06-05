4c885585e4cbe09ffe862b6a2dbaf9a3c36a1375
macqueen (Donald MacQueen) <macqueen1@llnl.gov>
2013-06-05 20:15:14 +0000

    Added software abstract from IM process
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rneshaps@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -47,5 +47,52 @@ echo $contents; } ?>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
+
+<b>Software Abstract<br>
+May 15, 2013
+</b>
+<br><br>LLNL-CODE-637312
+<DL>
+<DT>1a.	Identification
+<DD>acronym: Rneshaps<br>
+short: Functions for working with CAP88-PC version 1 concentration output files
+<DT>2.	Author name and affiliation
+<DD>Donald H. MacQueen, Lawrence Livermore National Laboratory
+<DT>3.	Software completion date:
+<DD>Original code spring 2009; current package version spring 2013.
+<DT>4.	Brief description
+<DD>This software is used to combine output files from US EPA CAP88-PC software (http://www.epa.gov/radiation/assessment/CAP88/aboutcap88.html) and estimate input parameters for CAP88-PC. Also includes features for tabular and graphical review of results.
+<DT>5.	Method of solution
+<DD>The software uses industry-standard nonlinear optimization routines to search for input parameters that provide a best fit of model output to data.
+<DT>6.	Computer(s) for which software is written
+<DD>Linux, Mac OS, Windows
+<DT>7.	Operating system:
+<DD>Any current or recent version of Linux, Mac OS, or Windows
+<DT>8.	Programming Languages used:
+<DD>R (95%), FORTRAN (5%)
+<DT>9.	Software limitations
+<DD>Available RAM, but this limit is unlikely to be encountered in typical usage. Software has been tested only on Mac OS, but R is multi-platform, so this software is expected to function on all platforms supported by R, provided users meet the requirements listed in item 11.
+<DT>10.	Unique features of the software
+<DD>Designed specifically for CAP88-PC output files, and for estimating CAP88-PC input parameters.
+<DT>11.	Related and auxiliary software
+<DD>Users must have R software and a compatible FORTRAN compiler.
+<DT>12.	Other programming or operating information or restrictions
+<DD>R software is licensed under GPL 2, so users must comply with GPL 2 license terms.
+<DT>13.	Hardware requirements
+<DD>Must support any recent Linux, Macintosh, or Windows operating system.
+<DT>14.	Time requirements
+<DD>Longest run time encountered to date is a few minutes, depending on what element of the software package is being used.
+<DT>15.	References
+<DD>
+(group 1)<br>
+A Best Fit Approach to Estimating Multiple Diffuse Source Terms Using Ambient Air Monitoring Data and an Air Dispersion Model, LLNL-JRNL-608237-DRAFT; accepted for publication in Operational Radiation Safety, March 2013.
+<br><br>
+(group 2)<br>
+R Core Team. R: A language and environment for statistical computing. R Foundation for Statistical Computing, Vienna, Austria. ISBN 3-900051-07-0. URL http://www.R-project.org/; 2012.
+<br>
+U.S. Environmental Protection Agency. User's guide for CAP88-PC Version 1. EPA 402-B-92-001; 1992. Available at http://www.epa.gov/rpdweb00/docs/cap88/402-b-92-001.pdf. Accessed 20 December 2012.
+</DL>
+
+
 </body>
 </html>
